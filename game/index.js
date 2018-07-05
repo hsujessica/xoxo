b084bb6a431675a30c57fa2cbce7652a913f8503
Jessica <23249301+hsujessica@users.noreply.github.com>
2018-07-05 14:31:02 -0400

    switching

--- game/index.js
+++ game/index.js
@@ -1,6 +1,22 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const board = Map();
+
+const move = (turn, position) => ({
+ type: 'MOVE',
+ turn: turn,
+ position: position,
+});
+
+export default function reducer(state = {turn: 'X', board: board}, action) {
+  switch (action.type) {
+    case 'START':
+      return state;
+    case 'MOVE':
+      console.log('hello!!!!!!!!!');
+      board.setIn(action.position, action.turn);
+      return {...state, turn: action.turn === 'X' ? 'O' : 'X'};
+    default:
+      return state;
+  }
 }
